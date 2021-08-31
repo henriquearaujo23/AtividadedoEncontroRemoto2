17f9a19b40f2a05cf0684474f7880237552146fc
Henrique Araújo <henriquearaujo49@hotmail.com>
2021-08-31 20:46:25 -0300

    Atividade criada

---
+++ sistema.js
@@ -0,0 +1,51 @@
+// (Exemplo de forof)
+// let nomesAlunos = ["Rafael", "Handryo", "Gabriel", "Ana"]
+
+// for (let nome of nomesAlunos) {
+//     console.log (`Esta pessoa se chama ${nome}`);
+// }
+
+
+//(Exemplo de forEach)
+// let nomesAlunos = ["Rafael", "Handryo", "Gabriel", "Ana"]
+// (forEach)
+// nomesAlunos.forEach(nome => {
+//     console.log(`O nome é ${nome}`);
+// });
+
+
+// // (Com While)
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while (contador <= numeroDeAlunos) {
+//     if (contador == 0) {
+//         console.log("O número atual é ZERO")
+//     } else if (contador % 2 !== 0) {
+//         console.log(`O número ${contador} é ÍMPAR`)
+//     } else {
+//         console.log(`O número ${contador} é PAR`)
+//     }
+
+//     contador++;
+// }
+    
+
+// (Primeiro exemplo)
+
+// let numeroDeAlunos = 10;
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     // console.log(contador)
+    
+//     if (contador == 0) {
+//         console.log("O número atual é ZERO")
+//     } else if (contador % 2 == 0) {
+//         console.log("O número " + contador + " é PAR")
+//     } else {
+//         console.log(`O número ${contador} é ÍMPAR`)
+//     }
+
+// }
